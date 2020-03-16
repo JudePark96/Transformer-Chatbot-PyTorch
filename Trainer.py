550b8c04e85d9db0b1af917b8bf3dca5fd22d63d
JudePark96 <judepark@kookmin.ac.kr>
2020-03-16 20:33:25 +0900

    Added train, evaluate function #6

--- Trainer.py
+++ Trainer.py
@@ -3,36 +3,85 @@ Author: Jude Park <judepark@kookmin.ac.kr>
 """
 
 
-from config.base_experiment_config import get_base_config
+from config.base_experiment_config import get_base_config, get_device_setting
 from utils.build_vocab import Vocabulary
 from torch.utils.data import DataLoader
+from utils.data_utils import load_data
+from utils.dataloder import get_loader
 from torch.nn import CrossEntropyLoss
+from model.transformer import Net
 from torch.nn import Module
 from tqdm import tqdm
 
 
+import torch.optim as optim
+import torch as T
+
 
 class Trainer(object):
 
-    def __init__(self, args: dict, vocab: Vocabulary, model: Module) -> None:
+    def __init__(self,
+                 args: dict,
+                 vocab: Vocabulary,
+                 model: Module,
+                 optimizer: optim,
+                 output_path: str) -> None:
         super().__init__()
 
-        if vocab.get_vocab_size() == args['vocab_size']:
+        if vocab.get_vocab_size() != args['vocab_size']:
             raise ValueError('vocabulary has not been initiated.')
 
         self.args = args
         self.vocab = vocab
-        self.model = model
+        self.model = model.to(get_device_setting())
+        self.optimizer = optimizer
+        self.output_path = output_path
+        self.train_loss = []
 
-    def train(self, epoch: int, loader: DataLoader) -> None:
-        # define what loss function is
-        loss_fn = CrossEntropyLoss(ignore_index=self.args['pad_idx'])
+        # define what loss function is.
+        self.loss_fn = CrossEntropyLoss(ignore_index=self.args['pad_idx'])
+
+    def train(self, epoch: int, train_loader: DataLoader, valid_loader: DataLoader) -> None:
+
+        # set the model to train mode.
+        self.model.train()
 
         for ep_iter in tqdm(range(1, epoch + 1)):
-            for i, (question, answer) in tqdm(loader):
-                pass
-            pass
-        pass
+            print(f'********** epoch number: {ep_iter} **********')
+            for i, (question, answer) in tqdm(enumerate(train_loader)):
+                self.optimizer.zero_grad()
+                output = self.model(question, answer)
+                output = output.view(-1, output.size(-1))
+                answer = answer.view(-1).long()
+                loss = self.loss_fn(output, answer)
+                print(f'********** training loss: {loss.item()} **********')
+                loss.backward()
+                self.optimizer.step()
 
-    def evaluate(self, loader: DataLoader):
-        pass
+                if (i + 1) % 100:
+                    self.evaluate(i, valid_loader)
+                    T.save(self.model.state_dict(), self.output_path + f'model-{ep_iter}-{i}.pt')
+
+    def evaluate(self, iteration: int, valid_loader: DataLoader):
+        print(f'********** evaluating start **********')
+
+        self.model.eval()
+
+        for i, (question, answer) in tqdm(enumerate(valid_loader)):
+            output = self.model(question, answer)
+            output = output.view(-1, output.size(-1))
+            answer = answer.view(-1).long()
+            loss = self.loss_fn(output, answer)
+            print(f'********** evaluating loss: {loss.item()} **********')
+
+
+if __name__ == '__main__':
+    train, valid, train_y, valid_y, corpus = load_data('./rsc/data/chatbot_korean.csv')
+    vocab = Vocabulary(corpus)
+    vocab.build_vocab()
+    loader = get_loader(train, train_y, vocab, 64, 32, True)
+    args = get_base_config()
+
+    model = Net(args)
+    optimizer = optim.Adam(params=model.parameters(), lr=args['lr'])
+    Trainer(args, vocab, model, optimizer).train(1, loader)
